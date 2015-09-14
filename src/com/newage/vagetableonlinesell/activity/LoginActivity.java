a8429aad40a4b5ca9c4c46cd5be69ba5535cd5d1
xuejun's pc <GFX123321@126.com>
2015-09-14 21:53:26 +0800

    9.14  21:53

--- src/com/newage/vagetableonlinesell/activity/LoginActivity.java
+++ src/com/newage/vagetableonlinesell/activity/LoginActivity.java
@@ -1,12 +1,55 @@
 package com.newage.vagetableonlinesell.activity;
 
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import com.xu.activity.XuBaseActivity;
 
-public class LoginActivity extends XuBaseActivity {
+public class LoginActivity extends XuBaseActivity implements OnClickListener {
+	private EditText mAccount, mPassword;
+	private CheckBox mRememberPassword;
+	private TextView mRegister, mForgetPass;
+	private Button mLogin;
 
 	@Override
 	public void setLayout() {
 		setContentView(R.layout.activity_login);
+		mAccount = (EditText) findViewById(R.id.account);
+		mPassword = (EditText) findViewById(R.id.password);
+		mRememberPassword = (CheckBox) findViewById(R.id.rememberPassword);
+		mRegister = (TextView) findViewById(R.id.register);
+		mLogin = (Button) findViewById(R.id.login);
+
+		mLogin.setOnClickListener(this);
+		mRegister.setOnClickListener(this);
+		mForgetPass.setOnClickListener(this);
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.login:
+
+			break;
+		case R.id.register:
+			Intent intent = new Intent(LoginActivity.this,
+					RegisterActivity.class);
+			startActivityForResult(intent, 10);
+			
+			break;
+		case R.id.forgetPassword:
+
+			break;
+
+		default:
+			break;
+		}
 
 	}
 
---
+++ src/com/newage/vegetableonlinesell/bean/Constant.java
@@ -0,0 +1,5 @@
+package com.newage.vegetableonlinesell.bean;
+
+public class Constant {
+
+}
