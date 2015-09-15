5dbdf499250d6a06a2212385bfe1ee6bddced107
xuejun's pc <GFX123321@126.com>
2015-09-15 23:08:40 +0800

    9.15 23:08

--- src/com/newage/vagetableonlinesell/activity/FruitListActivity.java
+++ src/com/newage/vagetableonlinesell/activity/FruitListActivity.java
@@ -1,22 +1,29 @@
 package com.newage.vagetableonlinesell.activity;
 
+import java.io.File;
+
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.LinearLayout;
+import cn.bmob.v3.datatype.BmobFile;
 
 import com.xu.activity.XuBaseActivity;
 
 //水果列表
-public class FruitListActivity extends XuBaseActivity implements OnClickListener{
+public class FruitListActivity extends XuBaseActivity implements
+		OnClickListener {
 	LinearLayout mBack;
+
 	@Override
 	public void setLayout() {
 		// TODO Auto-generated method stub
 		setContentView(R.layout.activity_fruit);
 		mBack = (LinearLayout) findViewById(R.id.back);
-		
+		// File file=new File(uri)
 		mBack.setOnClickListener(this);
+	
 	}
+
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
@@ -27,7 +34,7 @@ public class FruitListActivity extends XuBaseActivity implements OnClickListener
 		default:
 			break;
 		}
-		
+
 	}
-	
+
 }
